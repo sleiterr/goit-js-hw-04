1a00ba8d38a2e986924c4f58015372ac332d08b6
Oleg Troian <sleite4r@gmail.com>
2024-04-07 00:07:36 +0200

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,5 @@
+//* Task 1 Задача 1. Пакування товарів
+
 function isEnoughCapacity(products, containerSize) {
   const keys = Object.values(products);
   let totalSize = 0;
@@ -8,9 +10,6 @@ function isEnoughCapacity(products, containerSize) {
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
-
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
-
 console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
-
 console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
